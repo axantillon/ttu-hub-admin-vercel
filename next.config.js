cf2cc2af37f90b369b3b3fbbe11f29a20c687f77
Andres Antillon <aantillon.leiva@gmail.com>
2024-08-27 13:20:15 -0600

    hopefully make pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 // @ts-check
 
-module.exports = {
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
+  // Your Next.js config
   images: {
     loader: "custom",
     loaderFile: "./src/lib/utils/imageLoader.ts",
   },
-};
+});
